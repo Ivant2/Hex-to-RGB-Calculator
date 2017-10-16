a4a7a2f893850ef054e1e708c87074eb6d622e63
Ivant2 <32110248+Ivant2@users.noreply.github.com>
2017-10-16 13:36:22 -0600

    Comments

--- HextoRGBCalculator/ViewController.cs
+++ HextoRGBCalculator/ViewController.cs
@@ -21,19 +21,23 @@ namespace HextoRGBCalculator
 
         void ConvertButton_TouchUpInside(object sender, EventArgs e)
         {
+            //Turns Hex Values and RGB Values into strings
             string hexValue = hexValueTextField.Text;
             string redHexValue = hexValue.Substring(0, 2);
             string greenHexValue = hexValue.Substring(2, 2);
             string blueHexValue = hexValue.Substring(4, 2);
 
+            //Turn the Strings into integers
             int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber); 
             int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
             int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
 
+            //Sends the value of each integer to their respective Labels
             redValueLabel.Text = redValue.ToString();
             greenValueLabel.Text = greenValue.ToString();
             blueValueLabel.Text = blueValue.ToString();
 
+            //Changes colour depending on the Hex value inputted
             colorView.BackgroundColor = UIColor.FromRGB(redValue, greenValue, blueValue);
         }
 
